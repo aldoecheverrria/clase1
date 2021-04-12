20ecd9a760bca01473013a2ba23831b2408a0ff3
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-04-11 19:50:16 -0500

    primera parte de clase 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+      <div className="logo">Tema 1</div>
+      <div className="nav-items">
+        <a href="/">Definición</a>
+        <a href="/">Planteamiento</a>
+        <a href="/">Enunciado</a>
+      </div>
+    </nav>
+    <section className="hero">
+      
+    <div className="hero-container">
+          <div className="column-left">
+            <h1>Conceptos básicos para el análisis y resolución de problemas</h1>
+            <p>Esta sección contiene 3 partes</p>
+            <ul>
+              <li>Definición</li>
+              <li>Planteamiento</li>s
+              <li>Eunciado</li>
+
+            </ul>
+            <button>A darle</button>
+          </div>
+          <div className="column-right">
+            <div className="gfx">
+              <div className="gfx1"></div>
+              <h3>Elemento 1</h3>
+              <p>Elemento 1</p>
+            </div>
+            <div className=" gfx ">
+              <div className="gfx2"></div>
+            <h3>Elemento 2</h3>
+              <p>Elemento 2</p>
+            </div>
+            <div className="gfx ">
+              <div className="gfx3"></div>
+            <h3>Elemento 2</h3>
+              <p>Elemento 2</p>
+            </div>
+            <div className="gfx ">
+              <div className="gfx4"></div>
+            <h3>Elemento 2</h3>
+              <p>Elemento 2</p>
+            </div>
+          </div>
+          
+        </div>
+    </section>
+    </>
   );
 }
 
 export default App;
+
+
+/*
+
+<nav>
+      <div>TEMA 1</div>
+      <div className="nav-items">
+        <a href="/">Definición</a>
+        <a href="/">Planteamiento</a>
+        <a href="/">Enunciado</a>
+      </div>
+      <section>
+        
+      </section>
+    </nav>
+
+*/
